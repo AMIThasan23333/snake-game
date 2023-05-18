7b4b7885e2db86ee6c2b0f3aaed41d345ddf7faf
My Name <cpgsom819@gmail.com>
2023-05-19 00:05:11 +0800

    changeFoodPosition

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const playBoard = document.querySelector(".play-board") 
+
+
+ const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random() *30) + 1;
+    foodY = Math.floor(Math.random() *30) + 1;
+ }
+
+
+const initGame = () => {
+    let htmlMarkup = `
+    <div class="food" style="grid-area : ${foodY} / ${foodX} "></div>
+`
+    playBoard.innerHTML = htmlMarkup;
+}
+changeFoodPosition();
+initGame();
